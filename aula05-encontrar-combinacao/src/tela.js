2415ea8a9ce7bddc9291f2499f9f042b538863e3
Ljuniorcode <lucianojuniorarl@gmail.com>
2020-07-31 21:51:28 -0300

    03 - encontrar combinação das cartas, finalizando aula05

--- aula05-encontrar-combinacao/src/tela.js
+++ aula05-encontrar-combinacao/src/tela.js
@@ -5,18 +5,42 @@ class Tela {
     static obterCodigoHtml(item){
         return `
         <div class="col-md-3">
-            <div class="card" style="width: 50%">
+            <div class="card" style="width: 50%;" onclick="window.verificarSelecao('${item.id}','${item.nome}')">
                 <img src="${item.img}" name="${item.nome}" class="card-img-top" alt="...">
             </div>
             <br />
         </div>
         `
     }
+    
+    static configurarBotaoVerificarSelecao(funcaoOnClick){
+        window.verificarSelecao = funcaoOnClick
+    }
 
     static alterarConteudoHtml(codigoHtml){
         const conteudo = document.getElementById(ID_CONTEUDO)
         conteudo.innerHTML = codigoHtml
     }
+    
+    embaralhar(){
+        const copias = this.heroisIniciais
+        //duplicar os itens com o concat()
+        .concat(this.heroisIniciais)
+        //entrar em cada item e gerar um id aleatório
+        .map(item => {
+            return Object.assign({}, item, {id: Math.random() / 0.5})
+        })
+        //ordenar
+        .sort(()=> Math.random() - 0.5)
+
+        this.tela.atualizarImagens(copias)
+
+        setTimeout(()=>{
+            this.enconderHerois(copias)
+        },1000)
+       
+    }
+
 
     static gerarStringHTMLPelaImagem(itens){
         //para cada item da lista, vai executar a função obtercodigohtml
